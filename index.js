73eb399126a8fe51f67cf792323a334576c4103a
Pushpender <Pushpenderjangra@2005@gmail.com>
2024-06-29 21:50:05 +0530

    added ask_approval

--- index.js
+++ index.js
@@ -33,6 +33,73 @@ app.command('/hello', async ({ ack, body, client }) => {
   }
 })
 
+app.command('/approval-test', async ({ ack,body, client }) => {
+  // Acknowledge the command request
+  await ack();
+  try {
+    const requesterId = body.user_id; // Get the user ID of the requester
+    const view = {
+      type: 'modal',
+      callback_id: 'approval_request',
+      private_metadata: requesterId, // Pass a parameter
+      title: { type: 'plain_text', text: 'Approval Request' },
+      blocks: [
+        { type: 'divider' },
+        {
+          type: 'input',
+          block_id: 'approver_select',
+          element: {
+            type: 'users_select', // Select a user from the list of users
+            placeholder: { type: 'plain_text', text: 'Select Approver' },
+            action_id: 'selected_user',
+          },
+          label: { type: 'plain_text', text: 'Approver' }
+        },
+        {
+          type: 'input',
+          block_id: 'approval_text',
+          label: { type: 'plain_text', text: 'Approval Text' },
+          element: { type: 'plain_text_input', action_id: 'approval_message' }
+        },
+      ],
+      submit: { type: 'plain_text', text: 'Submit' }
+    };
+
+   const message= await client.views.open({ trigger_id: body.trigger_id, view });
+   console.log(message);
+  } catch (error) {
+    console.error(error);
+  }
+  
+})
+// View submission handler
+app.view('approval_request', async ({ ack, body, view, client }) => {
+  // Acknowledge the view_submission event and close the modal view 
+  //Input values from the  view
+  await ack();
+  const approverId = view.state.values.approver_select.selected_user.selected_user; // Get the selected approver
+  const approvalText = view.state.values.approval_text.approval_message.value; // Get the approval text
+  const requesterId = body.user.id; // Get the user ID of the requester 
+  const message = `
+  *Approval Request*
+  From: <@${body.user.id}>
+  Message: ${approvalText}
+  `;
+
+  const sentMessage = await client.chat.postMessage({ // Send a message to the selected approver
+      channel: approverId, 
+      text: message,
+      blocks: [
+          { type: 'section', text: { type: 'mrkdwn', text: message } },
+          { type: 'actions', elements: [
+              { type: 'button', text: { type: 'plain_text', text: 'Approve'}, style: 'primary', value: requesterId, action_id:'approve' },
+              { type: 'button', text: { type: 'plain_text', text: 'Reject' }, style: 'danger', value: requesterId ,action_id:'reject'},
+          ]}
+      ]
+  });
+  console.log(sentMessage);
+});
+
 
 const serverStart = async () => {
   // Start the app
